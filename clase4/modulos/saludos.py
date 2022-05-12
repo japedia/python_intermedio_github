235c3f82517e8496ed9ba19fd7a5fc72dc85d001
japedia <105305948+japedia@users.noreply.github.com>
2022-05-11 19:42:26 -0500

    se definieron dos funciones

--- clase4/modulos/saludos.py
+++ clase4/modulos/saludos.py
@@ -1,2 +1,11 @@
 def saludo():
     print("hola")
+
+
+def saludo_nombre(nombre):
+        print(f"Hola {nombre}")
+
+def saludo_nombre_completo(nombre,apellido_paterno,apellido_materno):
+    print(f"Hola {nombre} {apellido_paterno} {apellido_materno}")
+
+nombre="nombre"
