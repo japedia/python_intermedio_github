70be927bced2dc9064564b4096d2e18d74d45847
japedia <105305948+japedia@users.noreply.github.com>
2022-05-16 14:10:38 -0500

    ejemplo de alias

--- clase4/modulos/main.py
+++ clase4/modulos/main.py
@@ -5,8 +5,16 @@
 #saludo_nombre("Juan")
 #saludo_nombre("Pako")
 
-from saludos import *
+#from saludos import *
+#saludo()
+#saludo_nombre("Luis")
+#saludo_nombre_completo("Luis","Perez")
 
-saludo()
-saludo_nombre("Luis")
-saludo_nombre_completo("Luis","Perez")
+#paquete saludos_miguel
+#paquete saludos_sarai
+
+#ejemplo de alias
+#from saludos_miguel import saludos as saludo1
+#from saludos_sarai  import saludos as saludo2
+#saludo1
+
