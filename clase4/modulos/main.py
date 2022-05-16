f6feb8daafc76bf83091cfbc89574e21bf3e2092
japedia <105305948+japedia@users.noreply.github.com>
2022-05-16 14:07:18 -0500

    ejemplo importacion global

--- clase4/modulos/main.py
+++ clase4/modulos/main.py
@@ -1,8 +1,12 @@
 #import saludos
 #saludos.saludo()
 
+#from saludos import saludo_nombre
+#saludo_nombre("Juan")
+#saludo_nombre("Pako")
 
-from saludos import saludo_nombre
+from saludos import *
 
-saludo_nombre("Juan")
-saludo_nombre("Pako")
+saludo()
+saludo_nombre("Luis")
+saludo_nombre_completo("Luis","Perez")
